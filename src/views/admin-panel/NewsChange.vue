e197f0faf0444a4ff7172c7a6d49bd3f121c83b9
LeraNakahara <zhilkina-04@mail.ru>
2024-09-16 09:24:18 +0300

    изменения

--- src/views/admin-panel/NewsChange.vue
+++ src/views/admin-panel/NewsChange.vue
@@ -11,7 +11,7 @@ import ListOfActions from './ListOfActions.vue'
                 <a href="" class = "flex text-center w-[120px] h-[48px] items-center justify-center bg-slate-300 rounded-sm border-2 ml-[3px] mr-[3px] border-neutral-600 "><span>Мнения</span></a>
                 <a href="" class = "flex text-center w-[120px] h-[48px] items-center justify-center bg-blue-300 rounded-sm border-2 ml-[3px] mr-[3px]  border-blue-500"><span>Интервью</span></a>
                 <div class = "flex ml-[50%] mt-[6px]">
-                    <button class = 'text-[16px]  w-[180px] rounded-md ml-[10px] h-[48px] bg-primary text-white'>Отображать статтусы</button>
+                    <button class = 'text-[16px]  w-[180px] rounded-md ml-[10px] h-[48px] bg-primary text-white'>Назад</button>
                 </div>
             </div>
             <div class = "flex flex-row ml-[5px] mt-[5px]">
@@ -25,7 +25,8 @@ import ListOfActions from './ListOfActions.vue'
                 <input type="file" name="" id="">
             </div>
             <div class = "flex mt-[5px]">
-                <input type="text"  class = "ml-[101px] w-[90%] h-[300px] border-2 border-primary">
+                <label class = "ml-[15px]">Текст</label>
+                <input type="text"  class = "ml-[55px] w-[90%] h-[300px] border-2 border-primary">
             </div>
             <div class = "flex mt-[5px] gap-5 ml-[100px]">
                 <input type="datetime-local" class = "border-2 border-primary rounded-md">
@@ -38,7 +39,8 @@ import ListOfActions from './ListOfActions.vue'
                 <label for="">Источник</label>
                 <input type="text" class = "border-2 border-primary rounded-md">
             </div>
-            <div class = "flex gap-[10%]">
+            <div class = "flex gap-[1%]">
+                <label class = "ml-6">Комментарий</label>
                 <input type="text" class = "border-2 border-primary ml-[100px] mt-[5px] w-[50%] h-[80px]">
                 <div class = "flex-col">
                 <label for="" class = "ml-[5px]">Ответственный</label>
